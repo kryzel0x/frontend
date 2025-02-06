427022d993147aba1d1b009aa87fd528499bcc4b
sandeep26899 <sandeepsj26899@gmail.com>
2025-02-06 22:36:50 +0530

    design: initial commit

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -6,7 +6,10 @@ import logo from "../../../assets/logo/logo.png";
 import smallLogo from "../../../assets/logo/small-logo-hd.png";
 import { aptosClient, KRYZEL_COIN } from "../../../core/constants";
 import { useKeylessAccounts } from "../../../core/useKeylessAccounts";
-import { callApiGetMethod } from "../../../redux/Actions/api.action";
+import {
+  callApiGetMethod,
+  callApiPostMethod,
+} from "../../../redux/Actions/api.action";
 import {
   setAptBalance,
   setAptDecimals,
@@ -23,6 +26,7 @@ import Login from "../modals/Login/Login";
 import "./Header.scss";
 import { handleGetUserStakes } from "../../../services/aptos.service";
 import { formatAmount } from "../../../services/common.service";
+import CongratulationsModal from "../modals/CongratulationsModal/CongratulationsModal";
 
 const Header = () => {
   const dispatch: AppDispatch = useAppDispatch();
@@ -42,6 +46,7 @@ const Header = () => {
   const handleScroll = () => {
     setScroll(window.scrollY);
   };
+  const [showCong, setShowCong] = useState(false);
 
   useEffect(() => {
     const handleGetProfile = async () => {
@@ -123,114 +128,146 @@ const Header = () => {
     setImageSrc(userDetails?.profileImage?.split("=")[0]);
   };
 
+  useEffect(() => {
+    const handleUpdateWalletAddress = async () => {
+      const dataToSend = {
+        walletAddress: activeAccount?.accountAddress.toString(),
+      };
+
+      const res: any = await dispatch(
+        callApiPostMethod(APIURL.PROFILE, dataToSend, {}, false, false)
+      );
+
+      if (!res.error) {
+        dispatch(
+          setUserDetails({
+            ...userDetails, // Spread existing userDetails
+            walletAddress: dataToSend?.walletAddress, // Update only walletAddress
+          })
+        );
+        setShowCong(true);
+      }
+    };
+
+    if (!userDetails?.walletAddress && activeAccount) {
+      handleUpdateWalletAddress();
+    }
+  }, [activeAccount, dispatch, userDetails]);
+
   return (
-    <header
-      className={clsx(
-        "header",
-        scroll > 0 && "scrolled",
-        isLogin && "logged_in"
-      )}
-    >
-      <Container>
-        <div className="header_in">
-          <Link to={ROUTES.HOME} className="logo">
-            <img className="d-sm-inline-block d-none" src={logo} alt="logo" />
-            <img className="d-sm-none" src={smallLogo} alt="logo" />
-          </Link>
-          <div className="header_right">
-            {!isLogin ? (
-              <Button onClick={() => setShow(true)} className="login_btn">
-                Sign in
-              </Button>
-            ) : (
-              <>
-                <div className="header_nav">
-                  <ul>
-                    <li>
-                      <NavLink to={ROUTES.DASHBOARD}>Dashboard</NavLink>
-                    </li>
-                    <li>
-                      <NavLink to={ROUTES.LEADERBOARDS}>Leaderboard</NavLink>
-                    </li>
-                    <li>
-                      <NavLink to={ROUTES.GAME}>Game</NavLink>
-                    </li>
-                  </ul>
-                </div>
-                <div className="balance_txt">
-                  <img src={smallLogo} alt="logo" />
-                  {formatAmount(krzBalance)}
-                  <span>KRZ</span>
-                  {/* KRZ */}
-                </div>
-                <Dropdown>
-                  <Dropdown.Toggle
-                    ref={ref}
-                    className={clsx(
-                      "profile_btn",
-                      pathname.includes(ROUTES.PROFILE) && "active"
-                    )}
-                    onClickCapture={() => {
-                      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-                      document.body.clientWidth > 991 &&
-                        navigate(ROUTES.PROFILE);
-                    }}
-                  >
-                    <img
-                      src={imageSrc}
-                      alt="profile"
-                      referrerPolicy="no-referrer"
-                      onError={handleImageError}
-                    />
-                    <div>{userDetails?.name}</div>
-                    {/* <p>
-                                                <img src={coin} alt="coin" />
-                                            </p> */}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
+    <>
+      <header
+        className={clsx(
+          "header",
+          scroll > 0 && "scrolled",
+          isLogin && "logged_in"
+        )}
+      >
+        <Container>
+          <div className="header_in">
+            <Link to={ROUTES.HOME} className="logo">
+              <img className="d-sm-inline-block d-none" src={logo} alt="logo" />
+              <img className="d-sm-none" src={smallLogo} alt="logo" />
+            </Link>
+            <div className="header_right">
+              {!isLogin ? (
+                <Button onClick={() => setShow(true)} className="login_btn">
+                  Sign in
+                </Button>
+              ) : (
+                <>
+                  <div className="header_nav">
                     <ul>
                       <li>
-                        <NavLink
-                          onClick={() => ref.current?.click()}
-                          to={ROUTES.PROFILE}
-                        >
-                          Profile
-                        </NavLink>
+                        <NavLink to={ROUTES.DASHBOARD}>Dashboard</NavLink>
                       </li>
                       <li>
-                        <NavLink
-                          onClick={() => ref.current?.click()}
-                          to={ROUTES.DASHBOARD}
-                        >
-                          Dashboard
-                        </NavLink>
+                        <NavLink to={ROUTES.LEADERBOARDS}>Leaderboard</NavLink>
                       </li>
                       <li>
-                        <NavLink
-                          onClick={() => ref.current?.click()}
-                          to={ROUTES.LEADERBOARDS}
-                        >
-                          Leaderboard
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          onClick={() => ref.current?.click()}
-                          to={ROUTES.GAME}
-                        >
-                          Game
-                        </NavLink>
+                        <NavLink to={ROUTES.GAME}>Game</NavLink>
                       </li>
                     </ul>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </>
-            )}
+                  </div>
+                  <div className="balance_txt">
+                    <img src={smallLogo} alt="logo" />
+                    {formatAmount(krzBalance)}
+                    <span>KRZ</span>
+                    {/* KRZ */}
+                  </div>
+                  <Dropdown>
+                    <Dropdown.Toggle
+                      ref={ref}
+                      className={clsx(
+                        "profile_btn",
+                        pathname.includes(ROUTES.PROFILE) && "active"
+                      )}
+                      onClickCapture={() => {
+                        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                        document.body.clientWidth > 991 &&
+                          navigate(ROUTES.PROFILE);
+                      }}
+                    >
+                      <img
+                        src={imageSrc}
+                        alt="profile"
+                        referrerPolicy="no-referrer"
+                        onError={handleImageError}
+                      />
+                      <div>{userDetails?.name}</div>
+                      {/* <p>
+                                                <img src={coin} alt="coin" />
+                                            </p> */}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <ul>
+                        <li>
+                          <NavLink
+                            onClick={() => ref.current?.click()}
+                            to={ROUTES.PROFILE}
+                          >
+                            Profile
+                          </NavLink>
+                        </li>
+                        <li>
+                          <NavLink
+                            onClick={() => ref.current?.click()}
+                            to={ROUTES.DASHBOARD}
+                          >
+                            Dashboard
+                          </NavLink>
+                        </li>
+                        <li>
+                          <NavLink
+                            onClick={() => ref.current?.click()}
+                            to={ROUTES.LEADERBOARDS}
+                          >
+                            Leaderboard
+                          </NavLink>
+                        </li>
+                        <li>
+                          <NavLink
+                            onClick={() => ref.current?.click()}
+                            to={ROUTES.GAME}
+                          >
+                            Game
+                          </NavLink>
+                        </li>
+                      </ul>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </>
+              )}
+            </div>
           </div>
-        </div>
-      </Container>
-      <Login show={show} handleClose={() => setShow(false)} />
-    </header>
+        </Container>
+        <Login show={show} handleClose={() => setShow(false)} />
+      </header>
+      <CongratulationsModal
+        show={showCong}
+        handleClose={() => setShowCong(false)}
+      />
+    </>
   );
 };
 
--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -1,52 +1,49 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import MyStakes from "./MyStakes/MyStakes";
 import StakingStatement from "./StakingStatement/StakingStatement";
 import StartEarning from "./StartEarning/StartEarning";
-import { callApiPostMethod } from "../../../services/api.service";
-import { APIURL } from "../../../utils/constants";
-import { setUserDetails } from "../../../redux/slices/user.slice";
-import { UserDetails } from "../../../utils/utils";
-import { AppDispatch, RootState } from "../../../redux/store";
-import { useAppDispatch, useAppSelector } from "../../../utils/hooks";
-import { useKeylessAccounts } from "../../../core/useKeylessAccounts";
-import CongratulationsModal from "../../common/modals/CongratulationsModal/CongratulationsModal";
+// import { callApiPostMethod } from "../../../services/api.service";
+// import { APIURL } from "../../../utils/constants";
+// import { setUserDetails } from "../../../redux/slices/user.slice";
+// import { UserDetails } from "../../../utils/utils";
+// import { AppDispatch, RootState } from "../../../redux/store";
+// import { useAppDispatch, useAppSelector } from "../../../utils/hooks";
+// import { useKeylessAccounts } from "../../../core/useKeylessAccounts";
 import Poweredby from "../Homepage/PoweredBy/PoweredBy";
 
 const Dashboard = () => {
-  const dispatch: AppDispatch = useAppDispatch();
-  const { activeAccount } = useKeylessAccounts();
+  // const dispatch: AppDispatch = useAppDispatch();
+  // const { activeAccount } = useKeylessAccounts();
 
-  const userDetails = useAppSelector(
-    (state: RootState) => state.user.userDetails
-  ) as UserDetails;
+  // const userDetails = useAppSelector(
+  //   (state: RootState) => state.user.userDetails
+  // ) as UserDetails;
 
-  const [showCong, setShowCong] = useState(false);
+  // useEffect(() => {
+  //   const handleUpdateWalletAddress = async () => {
+  //     const dataToSend = {
+  //       walletAddress: activeAccount?.accountAddress.toString(),
+  //     };
 
-  useEffect(() => {
-    const handleUpdateWalletAddress = async () => {
-      const dataToSend = {
-        walletAddress: activeAccount?.accountAddress.toString(),
-      };
+  //     const res  : any= await dispatch(
+  //       callApiPostMethod(APIURL.PROFILE, dataToSend, {}, false, false)
+  //     );
 
-      const res  : any= await dispatch(
-        callApiPostMethod(APIURL.PROFILE, dataToSend, {}, false, false)
-      );
+  //     if (!res.error) {
+  //       dispatch(
+  //         setUserDetails({
+  //           ...userDetails, // Spread existing userDetails
+  //           walletAddress: dataToSend?.walletAddress, // Update only walletAddress
+  //         })
+  //       );
+  //       setShowCong(true);
+  //     }
+  //   };
 
-      if (!res.error) {
-        dispatch(
-          setUserDetails({
-            ...userDetails, // Spread existing userDetails
-            walletAddress: dataToSend?.walletAddress, // Update only walletAddress
-          })
-        );
-        setShowCong(true);
-      }
-    };
-
-    if (!userDetails?.walletAddress && activeAccount) {
-      handleUpdateWalletAddress();
-    }
-  }, [activeAccount, dispatch, userDetails]);
+  //   if (!userDetails?.walletAddress && activeAccount) {
+  //     handleUpdateWalletAddress();
+  //   }
+  // }, [activeAccount, dispatch, userDetails]);
 
   return (
     <>
@@ -54,10 +51,6 @@ const Dashboard = () => {
       <MyStakes />
       <StakingStatement />
       <Poweredby showIcon />
-      <CongratulationsModal
-        show={showCong}
-        handleClose={() => setShowCong(false)}
-      />
     </>
   );
 };
